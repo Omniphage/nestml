d743e98729a3ed65fa9b93a52b358b64521453ee
Philip Traeder <philip.traeder@rwth-aachen.de>
2018-03-28 00:35:37 +0200

    clean up NestDeclarationHelper
    -remove excess docstrings
    -remove parameter asserts
    -make pep484 conform
    -type hints
    -fix jinja2 models according to renamed methods
    
    Signed-off-by: Philip Traeder <philip.traeder@rwth-aachen.de>

--- pynestml/codegeneration/NestDeclarationsHelper.py
+++ pynestml/codegeneration/NestDeclarationsHelper.py
@@ -17,11 +17,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+from typing import List
+
+from pynestml.codegeneration.PyNestMl2NESTTypeConverter import NESTML2NESTTypeConverter
 from pynestml.modelprocessor.ASTDeclaration import ASTDeclaration
 from pynestml.modelprocessor.Symbol import SymbolKind
+from pynestml.modelprocessor.TypeSymbol import TypeSymbol
+from pynestml.modelprocessor.VariableSymbol import VariableSymbol
 from pynestml.utils.Logger import LOGGING_LEVEL, Logger
 from pynestml.utils.Messages import Messages
-from pynestml.codegeneration.PyNestMl2NESTTypeConverter import NESTML2NESTTypeConverter
 
 
 class NestDeclarationsHelper(object):
@@ -31,64 +35,36 @@ class NestDeclarationsHelper(object):
     nestml2NESTTypeConverter = None
 
     def __init__(self):
-        """
-        Initialized the declaration helper.
-        """
+        # type: () -> None
         self.nestml2NESTTypeConverter = NESTML2NESTTypeConverter()
         return
 
-    def getVariables(self, _astDeclaration=None):
-        """
-        For a given ast declaration it returns a list of all corresponding variable symbols.
-        :param _astDeclaration: a single ast declaration.
-        :type _astDeclaration: ASTDeclaration
-        :return: a list of all corresponding variable symbols.
-        :rtype: list(VariableSymbol)
-        """
-
-        assert (_astDeclaration is not None and isinstance(_astDeclaration, ASTDeclaration)), \
-            '(PyNestML.CodeGeneration.DeclarationsHelper) No or wrong type of declaration provided (%s)!' % type(
-                _astDeclaration)
+    @staticmethod
+    def get_variables(_ast_declaration=None):
+        # type: (ASTDeclaration) ->List[VariableSymbol]
         ret = list()
-        for var in _astDeclaration.getVariables():
-            symbol = _astDeclaration.getScope().resolveToSymbol(var.getCompleteName(), SymbolKind.VARIABLE)
+        for var in _ast_declaration.getVariables():
+            symbol = _ast_declaration.getScope().resolveToSymbol(var.getCompleteName(), SymbolKind.VARIABLE)
             if symbol is not None:
                 ret.append(symbol)
             else:
                 code, message = Messages.getCouldNotResolve(var.getCompleteName())
                 Logger.logMessage(_code=code, _message=message,
-                                  _errorPosition=_astDeclaration.getSourcePosition(), _logLevel=LOGGING_LEVEL.ERROR)
+                                  _errorPosition=_ast_declaration.getSourcePosition(), _logLevel=LOGGING_LEVEL.ERROR)
             return ret
 
-    def printVariableType(self, _variableSymbol=None):
-        """
-        Prints the type of the variable symbol to a corresponding nest representation.
-        :param _variableSymbol: a single variable symbol
-        :type _variableSymbol: VariableSymbol
-        :return: a string presentation of the variable symbol's type
-        :rtype: str
-        """
-        if _variableSymbol.hasVectorParameter():
-            return 'std::vector< ' + self.nestml2NESTTypeConverter.convert(_variableSymbol.getTypeSymbol()) + ' > '
+    def print_variable_type(self, _variable_symbol=None):
+        # type: (VariableSymbol) -> str
+        if _variable_symbol.hasVectorParameter():
+            return 'std::vector< ' + self.nestml2NESTTypeConverter.convert(_variable_symbol.getTypeSymbol()) + ' > '
         else:
-            return self.nestml2NESTTypeConverter.convert(_variableSymbol.getTypeSymbol())
+            return self.nestml2NESTTypeConverter.convert(_variable_symbol.getTypeSymbol())
 
-    def printSizeParameter(self, _astDeclaration=None):
-        """
-        Prints the size parameter of a single ast declaration.
-        :param _astDeclaration: a single ast declaration.
-        :type _astDeclaration: ASTDeclaration
-        :return: a string representation of the size parameter.
-        :rtype: str
-        """
-        return _astDeclaration.getSizeParameter()
+    @staticmethod
+    def print_size_parameter(_ast_declaration=None):
+        # type: (ASTDeclaration) -> str
+        return _ast_declaration.getSizeParameter()
 
-    def getDomainFromType(self, _typeSymbol=None):
-        """
-        Returns the domain for the handed over type symbol
-        :param _typeSymbol: a single type symbol
-        :type _typeSymbol: TypeSymbol
-        :return: the corresponding domain
-        :rtype: str
-        """
-        return self.nestml2NESTTypeConverter.convert(_typeSymbol)
+    def get_domain_from_type(self, _type_symbol=None):
+        # type: (TypeSymbol) -> str
+        return self.nestml2NESTTypeConverter.convert(_type_symbol)
